a4d8aaf11e194b5b0c4fef1a96e86f5d76177c88
BeknazarovTemur <imtemurb@gmail.com>
2024-04-03 15:42:56 +0500

    writed funtions in controller

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -6,8 +6,28 @@ use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    public function main()
+    public function home()
     {
+        return view('home');
+    }
 
+    public function about()
+    {
+        return view('about');
+    }
+
+    public function course()
+    {
+        return view('course');
+    }
+
+    public function teacher()
+    {
+        return view('teacher');
+    }
+
+    public function contact()
+    {
+        return view('contact');
     }
 }
